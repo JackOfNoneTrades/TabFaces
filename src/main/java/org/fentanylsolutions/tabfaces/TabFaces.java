53fc66ba0f9930aa7cf390a635f95945f91fbd30
jack <jack>
2025-04-12 16:05:46 +0200

    Removed @SideOnly annotation from mod class

--- src/main/java/org/fentanylsolutions/tabfaces/TabFaces.java
+++ src/main/java/org/fentanylsolutions/tabfaces/TabFaces.java
@@ -10,10 +10,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
-@SideOnly(Side.CLIENT)
 @Mod(
     modid = TabFaces.MODID,
     version = Tags.VERSION,
